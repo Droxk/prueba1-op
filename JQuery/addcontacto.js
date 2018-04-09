345e28d8d27ab0a9e1d71a128fe8013d4993e2ce
Diego Vp <dvillaverdeprado@gmail.com>
2018-04-09 14:04:36 +0200

    ajax

--- JQuery/addcontacto.js
+++ JQuery/addcontacto.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 	$("#btn_addcontacto").click(function(){
-		var idPost = "<?php echo $_POST[id] ?>";
+		var idPost = $('#id_cliente').val();
 		$("#btn_addcontacto").prop("disabled", true);
 
 		$("#div_contactoform").append('<form id="form_altacontacto" action="index.php" method="POST"></form>');
 		$("#form_altacontacto").append('<div class="form-group"><label class="control-label col-sm-2">Nombre</label><div class="col-sm-10"><input class="form-control" type="text" name="nombre_alta" id="input_nombrecontacto"></div></div>');
 		$("#form_altacontacto").append('<div class="form-group"><label class="control-label col-sm-2" for="pwd">Teléfono</label><div class="col-sm-10"><input class="form-control" type="number" name="tfno_alta" id="input_tfnocontacto"></div></div>');
 		$("#form_altacontacto").append('<input type="hidden" name="seleccion" value="alta_contacto">');
-		$("#form_altacontacto").append('<input type="hidden" name="id" value="<?php echo $_POST[id]; ?>">');
+		$("#form_altacontacto").append('<input type="hidden" name="id" value="'+idPost+'">');
 		$("#form_altacontacto").append('<div class="form-group"><div class="col-sm-offset-2 col-sm-10"><button type="submit" class="btn btn-success" id="boton_regcontacto" disabled>Registrar</button></div></div>');
+	});
+	
+	$("#div_contactoform").on('click', '#boton_regcontacto', function(e){
+		e.preventDefault();
+
+		$.ajax({
+			
+		});
+
+		console.log($('#input_nombrecontacto').val());
+		console.log($('#input_tfnocontacto').val());
 	});
 });
