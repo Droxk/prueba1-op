9c5e07756d349bf77aa63bd5652cd278ee57a10b
Diego Vp <dvillaverdeprado@gmail.com>
2018-04-10 14:58:38 +0200

    borrar cliente controlado

--- JQuery/clientes.js
+++ JQuery/clientes.js
@@ -1,25 +1,12 @@
 $(document).ready(function(){
 	// Este script contendra el control del borrado de los clientes ya que ahora mismo se borran sin aviso
 
-	// $('.borrar_contacto').click(function(e){
-	// 	console.log("borrar contacto");
-	// 	e.preventDefault();
-
-	// 	var del_id = $(this).siblings("input[name=id_contacto]").val();
- //        if(confirm("¿Borrar el contacto?")){
-	//         $.ajax({
-	// 	        type: "POST",
-	// 	        url: "Index.php",
-	// 	        data: { id_contacto: del_id, seleccion: "borrar_contacto" },
-	// 	        success: function(resp){
-	// 	        	console.log(resp);
-	// 	        }
- //        	});
- //        }
-
- //        $("#row_"+del_id).remove();
-
- //        return false;
-	// });
+	$('.borrar_cliente').click(function(e){
+       	
+       	if(!confirm("¿Borrar el contacto?")){
+       		e.preventDefault();
+       		console.log("cancelar");
+		}
+	});
 
 });
