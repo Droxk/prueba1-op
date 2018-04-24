a19beed76899a4a0ec18ce8ec1a2d378b8ca5fa5
Diego Vp <dvillaverdeprado@gmail.com>
2018-04-24 16:03:50 +0200

    cambios spaces

--- models/clientes_model.php
+++ models/clientes_model.php
@@ -175,12 +175,6 @@
                         // probar a reducir la i en 1 ($i-1) para ver si asi puedo coger el primer contacto del primer registro
 
                     }
-
-
-
-                    
-                    
-
                     // echo "<br>";
                 }
             }
@@ -388,7 +382,6 @@
             $pdf->setFontSubsetting(true);
 
             // Set font
-            // Set font
             // dejavusans is a UTF-8 Unicode font, if you only need to
             // print standard ASCII chars, you can use core fonts like
             // helvetica or times to reduce file size.
@@ -401,14 +394,6 @@
             // set text shadow effect
             $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
 
-            // Set some content to print
-//             $html = <<<EOD
-//             <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-//             <i>This is the first example of TCPDF library.</i>
-//             <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-//             <p>Please check the source code documentation and other examples for further information.</p>
-//             <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-// EOD;
             $this->conn = $this->db->conexion();
 
             $dataPDF = $this->db->get_contactos_por_cliente($this->conn, $id);
@@ -464,8 +449,6 @@
             ';
 
             $dataPDF = $this->todos;
-
-            //$html = $dataPDF[0]['nombre'];
 
             $this->db->close_con($this->conn);
 
